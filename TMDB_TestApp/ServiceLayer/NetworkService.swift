1bba7256aa13946738e2b184ac4350f13acc56b6
zagizik <63964521+zagizik@users.noreply.github.com>
2021-06-14 17:50:52 +0300

    handling data for several query types, pagination added, better image loader

--- TMDB_TestApp/ServiceLayer/NetworkService.swift
+++ TMDB_TestApp/ServiceLayer/NetworkService.swift
@@ -1,49 +1,84 @@
-import Foundation
+import UIKit
 
 protocol NetworkServiceProtocol {
-    func getTopRated(complition: @escaping (Result<[Movie]?, Error>) -> Void)
-    func fetchImage(from imagePath: String?, completion: @escaping (Result<Data?, Error>) -> Void)
-}
+    func getMovieList(list: movieList, page: Int, complition: @escaping (Result<[Movie]?, Error>) -> Void)
+    func downloadImage(urlString: String, completion: @escaping (Result<UIImage?, Error>) -> Void)}
 
 class NetworkService: NetworkServiceProtocol {
-    func getTopRated(complition: @escaping (Result<[Movie]?, Error>) -> Void) {
+    func getMovieList(list: movieList, page: Int = 0 ,complition: @escaping (Result<[Movie]?, Error>) -> Void) {
+        let pageString = String(page)
         var urlConstructor = URLComponents()
         urlConstructor.scheme = "https"
         urlConstructor.host = "api.themoviedb.org"
-        urlConstructor.path = "/3/movie/top_rated"
-        urlConstructor.queryItems = [URLQueryItem(name: "api_key", value: "a77fcc7c6a3a934746427df05b2abd6c")]
-
+        urlConstructor.path = "/3/movie/\(list.rawValue)"
+        urlConstructor.queryItems = [URLQueryItem(name: "api_key", value: "a77fcc7c6a3a934746427df05b2abd6c"),
+                                     URLQueryItem(name: "language", value: "ru-RU")]
+        
+        if pageString != "0" {
+            urlConstructor.queryItems?.append(URLQueryItem(name: "page", value: pageString))
+        }
+        
+        
         guard let url = urlConstructor.url else {return}
         URLSession.shared.dataTask(with: url) { data, _, error in
             if let error = error {
                 complition(.failure(error))
                 return
             }
-
+            
             do {
                 let movies = try JSONDecoder().decode(Movies.self, from: data!)
-                let result = movies.results
+                guard let result = movies.results else{ return }
                 complition(.success(result))
-
+                
             } catch {
                 complition(.failure(error))
             }
         }.resume()
     }
     
-    func fetchImage(from imagePath: String?, completion: @escaping (Result<Data?, Error>) -> Void ) {
-        let session = URLSession.shared
+    var imageCache = NSCache<NSString, UIImage>()
+    
+    func downloadImage(urlString: String, completion: @escaping (Result<UIImage?, Error>) -> Void) {
+        
+        if let cachedImage = imageCache.object(forKey: urlString as NSString) {
+            completion(.success(cachedImage))
+        } else {
+            guard let url = URL(string: "https://www.themoviedb.org/t/p/w440_and_h660_face/" + urlString) else { return }
+            let request = URLRequest(url: url, cachePolicy: URLRequest.CachePolicy.returnCacheDataElseLoad, timeoutInterval: 10)
+            let dataTask = URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
+                
+                guard error == nil,
+                      data != nil,
+                      let response = response as? HTTPURLResponse,
+                      response.statusCode == 200,
+                      let `self` = self else { completion(.failure(error!)); return }
+                
+                guard let image = UIImage(data: data!) else { return }
+                self.imageCache.setObject(image, forKey: url.absoluteString as NSString)
+                
+                DispatchQueue.main.async {
+                    completion(.success(image))
+                }
+            }
+            dataTask.resume()
+        }
+    }
+}
+
+extension UIImageView {
+
+    func load(from imagePath: String?) {
         guard let imagePath = imagePath else { return }
         let url = URL(string: "https://www.themoviedb.org/t/p/w440_and_h660_face/" + imagePath)
-            
-        let dataTask = session.dataTask(with: url!) { (data, response, error) in
-            if let error = error {
-                completion(.failure(error))
-                return
-            } else {
-                completion(.success(data))
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url!) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
             }
         }
-        dataTask.resume()
     }
 }
